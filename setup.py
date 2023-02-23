f17949a5a9c1d62d20b40c7078b1cced92b2393c
Phil Wang <lucidrains@gmail.com>
2023-02-23 14:16:17 -0800

    complete Don Diego Vega (Zorro without the mask)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'zorro-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'Zorro - Pytorch',
   author = 'Phil Wang',
@@ -21,6 +21,7 @@ setup(
     'beartype',
     'einops>=0.4',
     'torch>=1.6',
+    'torchaudio'
   ],
   classifiers=[
     'Development Status :: 4 - Beta',
