fa2cd8137c15d82417637050ec872ef15cdb6982
Phil Wang <lucidrains@gmail.com>
2023-06-26 14:33:51 -0700

    0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'zorro-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.6',
+  version = '0.1.0',
   license='MIT',
   description = 'Zorro - Pytorch',
   author = 'Phil Wang',
